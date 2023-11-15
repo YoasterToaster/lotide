27d38f2d445e5fcc6ca446294558f8f54dc73baf
YoasterToaster <joefoschia@gmail.com>
2023-11-15 17:43:36 -0500

    Added a counter to increment each item

---
+++ countOnly.js
@@ -0,0 +1,49 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  let count = 0;
+  for (const item of allItems) {
+    console.log(item);
+    
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+// assertEqual("Bootcamp", "Bootcamp");
+// assertEqual(5, 1);
+// assertEqual("Bootcamps", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("😊😊😊", "😊😊😊");
+// assertEqual("👌👌", "👌👌👌");
