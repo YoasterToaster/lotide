b0405205029c809a78945e22d6bdaaad01fc72a0
YoasterToaster <joefoschia@gmail.com>
2023-11-16 00:35:33 -0500

    eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -9,24 +9,17 @@ const assertEqual = function(actual, expected) {
 const historicalEvents = {
   frenchRevolution: "1789-1799",
   moonLanding: "July 20, 1969",
-  silkRoad: ["1st century BCE", "14th century CE"],
-  magnaCarta: {
-    year: 1215,
-    location: "Runnymede, England"
-  }
+  silkRoad: ["1st century BCE", "14th century CE"]
 };
 
 const famousPersonalities = {
   leonardoDaVinci: "1452-1519",
   cleopatra: "69 BCE - 30 BCE",
-  ameliaEarhart: "1897-1937",
-  ghengisKhan: {
-    birthYear: 1162,
-    deathYear: 1227
-  }
+  ameliaEarhart: "1897-1937"
 };
 
 const eqObjects = function(obj1, obj2) {
+  // Simple
   for (let key in obj1) {
     if (obj1[key] !== obj2[key]) {
       return false;
