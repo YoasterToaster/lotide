06c9f82d134f8afe64b555f64a625f59adb5c2b1
YoasterToaster <joefoschia@gmail.com>
2023-11-15 23:26:11 -0500

    findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -24,4 +24,4 @@ const findKeyByValue = function(obj, value) {
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, "Attack on Titan"), "anime");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), false);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), true);
