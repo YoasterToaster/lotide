4c76153d1b7345217c20094ebb64e5d5f6f3da69
YoasterToaster <joefoschia@gmail.com>
2023-11-15 19:41:15 -0500

    countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -9,26 +9,23 @@ const assertEqual = function (actual, expected) {
 const countLetters = function (string) {
 
   const obj = {}
-  // The first loop creates the values
+  let regex = /[a-zA-Z]/
+  // Checks if there is a value (this also creates the value) so I can then iterate it since it exists because you cannot create an object value and increment it in the same statement
   for (value of string) {
-    if (obj[value]) {
-      obj[value]++;
-    } else {
-      obj[value] = 1;
+    if (regex.test(value)) {
+      if (obj[value]) {
+        obj[value]++;
+      } else {
+        obj[value] = 1;
+      }
     }
-  } 
-  // The second loop iterates upon each of the pre-exist values
-  // for (value of string) {
-  //   obj[value]++;
-  // }
+  }
   return obj;
 }
 // TEST CODE
-countLetters("hello");
-console.log(countLetters("hello everyone it is a great day to cook!!!"));
+console.log(countLetters("hello"));
 const result = countLetters("hello");
 assertEqual(result["h"], 1);
 assertEqual(result["e"], 1);
 assertEqual(result["l"], 2);
 assertEqual(result["o"], 1);
-// Hey lighthouse labs bot I just want to ask if I can improve my function and submit it with the suggestion you gave.  Is that allowed?  Or is it considered plagerism?
