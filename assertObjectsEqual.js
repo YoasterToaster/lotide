2ccd31e22c411ac92249af20c53a6df1641ac376
YoasterToaster <joefoschia@gmail.com>
2023-11-16 01:35:00 -0500

    assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -27,19 +27,24 @@ const eqObjects = function(obj1, obj2) {
   return false;
 };
 
-const assertObjectsEqual = function(actual, expected){
+const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
-    console.log(`Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else if (!eqObjects(actual, expected)) {
-    console.log(`Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
-}
+};
 
 const historicalEvents = {
   frenchRevolution: "1789-1799",
   moonLanding: "July 20, 1969",
   silkRoad: ["1st century BCE", "14th century CE"]
+};
+const historicalEvents2 = {
+  frenchRevolution: "1789-",
+  moonLanding: "July  1969",
+  silkRoad: ["1st centuryCE", "14th century CE"]
 };
 
 const famousPersonalities = {
@@ -49,6 +54,7 @@ const famousPersonalities = {
 };
 
 assertObjectsEqual(historicalEvents, historicalEvents);
+assertObjectsEqual(historicalEvents, historicalEvents2);
 assertObjectsEqual(historicalEvents, famousPersonalities);
 assertObjectsEqual(historicalEvents, true);
 assertObjectsEqual(historicalEvents, false);
