bf1b6bee69e951666603121df81c11909f084ee3
YoasterToaster <joefoschia@gmail.com>
2023-11-15 19:44:53 -0500

    countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -10,7 +10,7 @@ const countLetters = function (string) {
 
   const obj = {}
   let regex = /[a-zA-Z]/
-  // Checks if there is a value (this also creates the value) so I can then iterate it since it exists because you cannot create an object value and increment it in the same statement
+  // Checks if value is a letter then creates the value so I can then iterate it since it exists because you cannot create an object value and increment it in the same statement
   for (value of string) {
     if (regex.test(value)) {
       if (obj[value]) {
