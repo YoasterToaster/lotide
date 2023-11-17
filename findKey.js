20cbba01b164463a553807acbf11078deda8b107
YoasterToaster <joefoschia@gmail.com>
2023-11-16 23:43:03 -0500

    findKey.js

---
+++ findKey.js
@@ -0,0 +1,95 @@
+const eqArrays = function (actual, expected) {
+  //If the lengths are not the same then it will return false
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const eqObjects = function (obj1, obj2) {
+  // Ill probably add more comments later but I am kinda tired and I console.log'd quite a ton.
+  if (Object.entries(obj1).length === Object.entries(obj2).length) {
+    for (let key in obj1) {
+      if (obj1[key] !== obj2[key] && !Array.isArray(obj1[key]) && !Array.isArray(obj2[key])) {
+        return false;
+      } else if (Array.isArray(obj1[key]) && Array.isArray(obj2[key]) && !eqArrays(obj1[key], obj2[key])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else if (!eqObjects(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+const findKey = (obj, callback) => {
+  for (let key in obj) {
+    if (obj[key] === undefined || obj[key] === null) {
+      return undefined;
+    } else if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertObjectsEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+
+assertObjectsEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 3), "Akaleri");
+
+assertObjectsEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 32 },
+  "noma": { stars: 21 },
+  "elBulli": { stars: 33 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "Ora");
+
+assertObjectsEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 4), undefined); // this returns undefined
