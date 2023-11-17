009f524e13b5df92e82c4e148126b032df207fa2
YoasterToaster <joefoschia@gmail.com>
2023-11-16 22:40:06 -0500

    takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,15 +1,37 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (!eqArrays(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  //If the lengths are not the same then it will return false
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
 const takeUntil = function(array, callback) {
   let newArray = [];
-  for (let value of array){
-    if (value == true){
+  for (let value of array) {
+    if (!callback(value)) {
       newArray.push(value);
     } else {
       return newArray;
     }
-    return newArray;
   }
-  return 
-}
+  return newArray;
+};
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
@@ -21,5 +43,9 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
+const data3 = [1, 2, 5, 7, 2, -1, 0, 2, 4, 5];
 
-//What am I supposed to do with the callback arguement in the function?  I am just a little confused, can you show an example?
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
+// Yes I know its a double equal sign but I meant to leave that there to see if it reached a falsey value
+assertArraysEqual(takeUntil(data3, x => x == false), [1, 2, 5, 7, 2, -1]);
