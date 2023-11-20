24ff061ad96d7168f18ef05cb2eb0d7f9570ef57
YoasterToaster <joefoschia@gmail.com>
2023-11-19 20:12:44 -0500

    tail.js

--- tail.js
+++ tail.js
@@ -7,7 +7,8 @@ const assertEqual = function (actual, expected) {
 };
 
 const tail = function (arr) {
-  let newArray = arr.slice(1);
+  let newArray = [];
+  newArray = arr.slice(1);
   return newArray;
 };
 let orignalArray = [0, 1, 2, 4];
@@ -17,7 +18,7 @@ console.log(tail([1, 2, 3]));
 assertEqual(orignalArray.length, 4);
 console.log(tail(["nooo", 2, "hello"]));
 assertEqual(tail([2, "hello"]).length, 1);
-assertEqual(orignalArray.length, 4);
 assertEqual(tail(["nooo", 2, "hello"])[1], "hello");
 assertEqual(tail([2, 'hello'])[0], "hello");
 assertEqual(tail(array1).length, 0);
+assertEqual(tail([]).length, 0);
