4130ed1416a38b4ae7ea877d1e54629fae1f5666
YoasterToaster <joefoschia@gmail.com>
2023-11-20 15:41:19 -0500

    used Mocha and Chai to improve the test functions of middleTtest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,23 @@
-const assertArraysEqual = require('../assertArraysEqual.js');
+const assert = require('chai').assert;
 const middle = require('../middle.js');
 
-console.log(middle([1, 2, 3, 4, 5, 6, 7]));
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2]), []);
+describe("#head", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns '3' for [5, 3, 1]", () => {
+    assert.deepEqual(middle([5, 3, 1]), [3]); 
+  });
+  it("returns 'wee' for [Bootcamp, wee, knee]", () => {
+    assert.deepEqual(middle(["Bootcamp", "wee", "knee"]), ["wee"]); 
+  });
+  it("returns '😊😊' for ['😊😊😊', '😊😊'. '😊😊😊']", () => {
+    assert.deepEqual(middle(["😊😊😊", "😊😊", "😊😊😊"]), ["😊😊"]); 
+  });
+  it("returns '👌👌👌' for ['👌👌', '👌👌👌', '👌👌']", () => {
+    assert.deepEqual(middle(['👌👌', '👌👌👌', '👌👌']), ['👌👌👌']); 
+  });
+});
