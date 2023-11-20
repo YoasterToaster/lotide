d0213106d3af755937567d436830ca167d1bc311
YoasterToaster <joefoschia@gmail.com>
2023-11-20 15:30:58 -0500

    used Mocha and Chai to improve the test functions of headTtest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,30 @@
-const head = require('../head.js')
-const assertEqual = require('../assertEqual.js')
+const head = require('../head.js');
+const assert = require('chai').assert;
+const assertEqual = require('../assertEqual.js');
 
-assertEqual(head(["Bootcamp", "wee", "knee"]), "Bootcamp");
-assertEqual(head([5, 3, 1]), 5);
-assertEqual(head(["Bootcamps", "Bootcamp"]), "Bootcamps");
-assertEqual(head([1, 2, 3]), 1);
-assertEqual(head(["😊😊😊", "😊😊"]), "😊😊😊");
-assertEqual(head(["👌👌", "👌👌👌"]), "👌👌👌");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns '5' for [5, 3, 1]", () => {
+    assert.strictEqual(head([5, 3, 1]), 5); 
+  });
+  it("returns 'Bootcamp' for [Bootcamp, wee, knee]", () => {
+    assert.strictEqual(head(["Bootcamp", "wee", "knee"]), "Bootcamp"); 
+  });
+  it("returns 'Bootcamp' for [Bootcamps, Bootcamp]", () => {
+    assert.strictEqual(head(["Bootcamps", "Bootcamp"]), 'Bootcamps'); 
+  });
+  it("returns '1' for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1); 
+  });
+  it("returns '😊😊😊' for ['😊😊😊', '😊😊]", () => {
+    assert.strictEqual(head(["😊😊😊", "😊😊"]), "😊😊😊"); 
+  });
+  it("returns '👌👌👌' for ['👌👌', '👌👌👌']", () => {
+    assert.strictEqual(head(['👌👌', '👌👌👌']), '👌👌'); 
+  });
+});
